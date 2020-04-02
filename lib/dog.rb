eab71312bb7ffa6c52933a317f6ad1a4a841eb41
Elias Taveras <eliastave12@gmail.com>
2020-04-02 07:06:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,28 +11,34 @@
 # end
 
 class Dog
-  attr_accessor :name
-  @@all = []
 
-  def initialize(name)
-      @name = name
-  end
+    attr_accessor :name
+    @@all = []
 
-  def self.all
-    @@all
-  end
+    def initialize(name)
+        @name = name
 
-  def self.clear_all
-    @@all.clear
-  end
-
-  def self.print_all
-    @@all.each do |dog|
-      puts dog.name
+        # @@all << self
+        save
     end
-  end
 
-  def save
-    @@all << self 
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+
+    end
+
+    def save
+        @@all << self 
+    end  
   end
-end
