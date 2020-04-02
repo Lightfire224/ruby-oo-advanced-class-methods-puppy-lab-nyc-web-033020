36ade6cb5171047a864b4890258020c5b6aeafa0
Elias Taveras <eliastave12@gmail.com>
2020-04-02 07:06:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,27 +15,24 @@ class Dog
   @@all = []
 
   def initialize(name)
-      @name = name
-
-        # @@all << self
-        save
+    @name = name
   end
 
   def self.all
     @@all
   end
 
-    def self.clear_all
-      @@all.clear
-    end
+  def self.clear_all
+    @@all.clear
+  end
 
-    def self.print_all
-      @@all.each do |dog|
-        puts dog.name
-      end
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
     end
+  end
 
-    def save
-      @@all << self 
-    end
+  def save
+    @@all << self 
+  end
 end
